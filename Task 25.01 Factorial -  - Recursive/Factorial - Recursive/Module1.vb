16116af6a628803542623f5cec9ea1c8526f6299
Huw William Mawson <32649003+huwmawson@users.noreply.github.com>
2019-03-01 09:17:10 +0100

    Add files via upload

---
+++ Task 25.01 Factorial -  - Recursive/Factorial - Recursive/Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Function Factorial(n) As Integer
+
+        Dim Result As Integer
+        'The base case will return 1
+        If n = 0 Then
+            Result = 1
+        Else
+            'the function will iterate through recursively until it reaches the base case
+            Result = n * Factorial(n - 1)
+        End If
+        Return (Result)
+
+    End Function
+
+    Sub Main()
+
+        Dim result As Integer
+
+        'call the function pasing an integer as the parameter n
+        result = Factorial(4)
+
+
+        Console.WriteLine(result)
+        Console.ReadKey()
+
+    End Sub
+
+
+End Module
---
+++ Task 25.03 Factorial - Recursive/Factorial - Recursive/Module1.vb
@@ -0,0 +1,47 @@
+﻿Module Module1
+    Dim CallNumber 'declare the global variable (available throughout the program) callNumber
+
+    Function Factorial(n) As Integer 'This is the function taking the parameter n s it's argument - it returns an integer
+
+        Dim Result As Integer
+
+        CallNumber += 1 ' incremnet the callNumber variable each time we run through the function
+
+        Console.WriteLine("The current call number is " & CallNumber & " " & "and the value of the number is " & n) ' output the callnumber and the current value f n as iti is passed to the function, to the console each time the function is called
+
+
+        If n = 0 Then
+            Result = 1 'The base case will return 1 (the factorial of 0)
+        Else
+            'the function will iterate through recursively until it reaches the base case as each parameter passed is the previous n value decremented by 1
+
+            Result = n * Factorial(n - 1) 'here is where the recursive call takes place, calling the function Factorial(n-1) from within the Factorial function
+        End If
+
+
+        Console.WriteLine("Return value: " & Result)
+
+        Return (Result)
+
+    End Function
+
+    Sub Main()
+
+        Dim result As Integer
+        Dim num As Integer = 4
+        CallNumber = 0 'initialise the callNumber to 0
+
+        'call the function pasing an integer as the parameter n, in this case 4
+
+
+        result = Factorial(num) 'This is the function call.
+        Console.WriteLine(vbCrLf & "The factorial of " & num & " is " & result) 'output the result ot the console
+
+
+        Console.ReadKey()
+
+    End Sub
+
+
+End Module
+
